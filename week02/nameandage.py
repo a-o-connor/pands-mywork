3150a7cad95135164e114216b17e6d19865eba8d
a-o-connor <157632616+a-o-connor@users.noreply.github.com>
2024-02-01 20:17:34 +0000

    Delete week02/nameandage.py

--- week02/nameandage.py
+++
@@ -1,7 +0,0 @@
-#Program that reads in name and age and says "Your name is X and your age is Y"
-#First input: What is your name?
-name = input("What is your name?")
-#Second input: What age are you? Only accepts numbers!
-age = int(input("What age are you?"))
-#Then print out a respnse after getting the answer from the user, say Hello name , you are age. \t adds a tab space before you are age
-print(f"Hello {name}, \t you are {age}")
