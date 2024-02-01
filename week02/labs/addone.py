0b3de8b2c3e28c204b925f9e66e581e4d9e982d5
Ailsa O'Connor <ailsa.oconnor@approcess.com>
2024-02-01 20:15:30 +0000

    Just tidying up

---
+++ week02/labs/addone.py
@@ -0,0 +1,6 @@
+#want this program to take an input number from user and add one to it 
+#first line will be asking for the input number, preface with int I want to only accept integers 
+number = int(input("Pick a number!"))
+newnumber = number + 1
+print(f"{number} plus one is {newnumber}") 
+#ensure to have the int() there or it will read as a string 
