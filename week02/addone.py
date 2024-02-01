4fa8c056a2ced3bbc7c1122cc3b6517556ea554d
a-o-connor <157632616+a-o-connor@users.noreply.github.com>
2024-02-01 20:17:12 +0000

    Delete week02/addone.py

--- week02/addone.py
+++
@@ -1,6 +0,0 @@
-#want this program to take an input number from user and add one to it 
-#first line will be asking for the input number, preface with int I want to only accept integers 
-number = int(input("Pick a number!"))
-newnumber = number + 1
-print(f"{number} plus one is {newnumber}") 
-
