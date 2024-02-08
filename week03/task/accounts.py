2ea64f5d67d7386c534c8df05034f8fe876d751d
aoconnor <oconnorails1@gmail.com>
2024-02-08 17:19:15 +0000

    made task directory for week 3

---
+++ week03/task/accounts.py
@@ -0,0 +1,3 @@
+# Weekly Task 03: 
+# Program that reads in a 10 character account number and outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs)
+# Author: A OConnor
